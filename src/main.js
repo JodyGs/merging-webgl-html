84999ac2fa10f2f7e8b15449141a9592ddf4e01c
JodyGs <demajn@tuta.io>
2023-03-03 13:01:30 +0100

    postprocessing + noise

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import Scroll from './scroll';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
+import noise from './shaders/noise.glsl'
+
+
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+
 
 
 
@@ -58,22 +66,71 @@ export default class Sketch {
 
       this.resize()
       this.setupResize()
+      this.composerPass()
       this.render()
     })
   }
 
+  composerPass() {
+    this.composer = new EffectComposer(this.renderer);
+    this.renderPass = new RenderPass(this.scene, this.camera);
+    this.composer.addPass(this.renderPass);
+
+    //custom shader pass
+    var counter = 0.0;
+    this.myEffect = {
+      uniforms: {
+        "tDiffuse": { value: null },
+        "uScrollSpeed": { value: null },
+        "uTime": { value: null },
+      },
+      vertexShader: `
+      varying vec2 vUv;
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix 
+          * modelViewMatrix 
+          * vec4( position, 1.0 );
+      }
+      `,
+      fragmentShader: `
+      uniform sampler2D tDiffuse;
+      varying vec2 vUv;
+      uniform float uScrollSpeed;
+      uniform float uTime;
+      ${noise}
+      void main(){
+        vec2 newUV = vUv;
+        float area = smoothstep(1.,0.8,vUv.y)*2. - 1.;
+        float area1 = smoothstep(0.4,0.0,vUv.y);
+        area1 = pow(area1,4.);
+        float noise = 0.5*(cnoise(vec3(vUv*10.,uTime/5.)) + 1.);
+        float n = smoothstep(0.5,0.51, noise + area/2.);
+        newUV.x -= (vUv.x - 0.5)*0.1*area1*uScrollSpeed;
+        gl_FragColor = texture2D( tDiffuse, newUV);
+      gl_FragColor = mix(vec4(1.),texture2D( tDiffuse, newUV),n);
+      }
+      `
+    }
+
+    this.customPass = new ShaderPass(this.myEffect);
+    this.customPass.renderToScreen = true;
+
+    this.composer.addPass(this.customPass);
+  }
+
   mouseMovement() {
-    window.addEventListener( 'mousemove', (event)=>{
-      this.mouse.x = ( event.clientX / this.width ) * 2 - 1;
-      this.mouse.y = - ( event.clientY / this.height ) * 2 + 1;
+    window.addEventListener('mousemove', (event) => {
+      this.mouse.x = (event.clientX / this.width) * 2 - 1;
+      this.mouse.y = - (event.clientY / this.height) * 2 + 1;
 
-      this.raycaster.setFromCamera( this.mouse, this.camera );
+      this.raycaster.setFromCamera(this.mouse, this.camera);
 
-      const intersects = this.raycaster.intersectObjects( this.scene.children );
+      const intersects = this.raycaster.intersectObjects(this.scene.children);
 
-      if(intersects.length>0){
-          let obj = intersects[0].object;
-          obj.material.uniforms.uHover.value = intersects[0].uv;
+      if (intersects.length > 0) {
+        let obj = intersects[0].object;
+        obj.material.uniforms.uHover.value = intersects[0].uv;
       }
     }, false)
   }
@@ -158,6 +215,8 @@ export default class Sketch {
     this.scroll.render()
     this.currentScroll = this.scroll.scrollToRender
     this.setPosition()
+    this.customPass.uniforms.uScrollSpeed.value = this.scroll.speedTarget
+    this.customPass.uniforms.uTime.value = this.time
 
     this.materials.forEach(mat => {
       mat.uniforms.uTime.value = this.time
@@ -166,6 +225,7 @@ export default class Sketch {
     // this.material.uniforms.uTime.value = this.time
 
     this.renderer.render(this.scene, this.camera)
+    this.composer.render()
     window.requestAnimationFrame(this.render.bind(this))
   }
 }
